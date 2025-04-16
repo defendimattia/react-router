22705f95b930110cd6117a1d55fe110e92a82578
defendimattia <defendi.mattia92@gmail.com>
2025-04-16 13:52:56 +0200

    add: fetch call and post rappresentation

--- src/pages/PostList.jsx
+++ src/pages/PostList.jsx
@@ -1,3 +1,24 @@
+import { useEffect, useState } from "react"
+
 export default function PostList() {
-    return <h1>postlist</h1>
+
+    const [list, setList] = useState([])
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/posts")
+            .then(response => response.json())
+            .then(data => setList(data))
+
+    }, [])
+
+    return (
+        <>
+            <h1>postlist</h1>
+            <ul>
+                {list.map(el => <li key={el.id}>{el.title}</li>)}
+            </ul>
+        </>
+
+    )
+
 }
