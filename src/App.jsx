aad415acf81f32b02f58be1d57a665a4c7eb41e3
defendimattia <defendi.mattia92@gmail.com>
2025-04-16 12:49:27 +0200

    add: pages and routes main structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Homepage from "./pages/Homepage"
+import AboutUs from "./pages/AboutUs"
+import PostList from "./pages/PostList"
 
 function App() {
   return (
     <>
+
+    <BrowserRouter>
+    <Routes>
+
+    <Route path="/" element={<Homepage />}/>
+    <Route path="/aboutus" element={<AboutUs />}/>
+    <Route path="/postlist" element={<PostList />}/>
+
+    </Routes>
+    </BrowserRouter>
+
     </>
   )
 }
