8b561eb09525700e6d466f41a6daaef9d781aebf
defendimattia <defendi.mattia92@gmail.com>
2025-04-16 14:04:58 +0200

    add: navbar as layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,22 @@
-import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Homepage from "./pages/Homepage"
 import AboutUs from "./pages/AboutUs"
 import PostList from "./pages/PostList"
 import "./App.css"
+import MainNav from "./layouts/MainNav"
 
 function App() {
   return (
     <>
-
       <BrowserRouter>
-        <nav>
-          <ul>
-            <li><NavLink to="/">homepage</NavLink></li>
-            <li><NavLink to="/aboutus">about us</NavLink></li>
-            <li><NavLink to="/postlist">post list</NavLink></li>
-          </ul>
-        </nav>
-
         <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/aboutus" element={<AboutUs />} />
-          <Route path="/postlist" element={<PostList />} />
+
+          <Route element={<MainNav />}>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/aboutus" element={<AboutUs />} />
+            <Route path="/postlist" element={<PostList />} />
+          </Route>
+          
         </Routes>
       </BrowserRouter>
 
