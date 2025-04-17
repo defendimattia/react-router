f11ae2d71547b6000a29e9bc5a4a8bcba6401912
defendimattia <defendi.mattia92@gmail.com>
2025-04-17 16:03:03 +0200

    add: loading logic to promises

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,8 +1,31 @@
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
 export default function PostPage() {
+
+    const [post, setPost] = useState(null)
+    const [loading, setLoading] = useState(true)
     const { id } = useParams()
 
-    return <h1>postpage {id}</h1>
+    useEffect(() => {
+        setLoading(true)
+        fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+            .then(response => response.json())
+            .then(data => {
+                setPost(data)
+                setLoading(false)
+            })
+    }, [])
+
+
+    if (loading) {
+        return <h1>Loading...</h1>
+    }
+
+    return <>
+        <h1>{post.title}</h1>
+        <p>{post.body}</p>
+    </>
+
 
 }
