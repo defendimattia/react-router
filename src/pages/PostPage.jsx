13c1cb64cbf462c48bda1871a4b6e1064fdb0b00
defendimattia <defendi.mattia92@gmail.com>
2025-04-17 16:25:37 +0200

    add: prev and next buttons on postpage

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,21 +1,24 @@
 import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 
 export default function PostPage() {
 
     const [post, setPost] = useState(null)
     const [loading, setLoading] = useState(true)
     const { id } = useParams()
+    const [url, setUrl] = useState(parseInt(id))
+    const navigate = useNavigate()
+
 
     useEffect(() => {
         setLoading(true)
-        fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+        fetch(`https://jsonplaceholder.typicode.com/posts/` + url)
             .then(response => response.json())
             .then(data => {
                 setPost(data)
                 setLoading(false)
             })
-    }, [])
+    }, [url])
 
 
     if (loading) {
@@ -25,6 +28,8 @@ export default function PostPage() {
     return <>
         <h1>{post.title}</h1>
         <p>{post.body}</p>
+        {url > 1 && <button onClick={() => { setUrl(prev => prev - 1) }}>prev</button >}
+        {url < 100 && <button onClick={() => { setUrl(prev => prev + 1) }}>next</button>}
     </>
 
 
